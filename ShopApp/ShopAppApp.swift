27fbeccbef6217a1ffe53b0c66e7ecaf3118bcb0
Alaa Gawish <53160167+alaagawish@users.noreply.github.com>
2023-10-23 02:16:09 +0300

    Initial Commit

---
+++ ShopApp/ShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppApp.swift
+//  ShopApp
+//
+//  Created by Alaa on 23/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
